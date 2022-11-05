3251caca7bc06a36e30c477706f2f81790595c16
Mariana Lima <mariana.lima118@outlook.com>
2022-11-05 16:14:50 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,34 @@
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "./src/components/Loading";
+import { THEME } from "./src/styles/theme";
+import { SignIn } from "./src/screens/SignIn";
+import { StatusBar } from "react-native";
+import { AuthContextProvider } from "./src/context/AuthContext";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
+    </NativeBaseProvider>
+  );
+}
